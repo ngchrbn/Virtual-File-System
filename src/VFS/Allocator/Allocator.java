c9fc7ec96ad015de6e3e86944752bf78c004b380
Guy Cherubin <ngchrbn@gmail.com>
2021-06-07 21:19:16 +0200

    Modified functions inside to implement VFS package

--- src/VFS/Allocator/Allocator.java
+++ src/VFS/Allocator/Allocator.java
@@ -1,7 +1,31 @@
 package VFS.Allocator;
 
+import VFS.VFileManager.Directory;
 import VFS.VFileManager._File;
 
 public interface Allocator {
+
+    /**
+     * Set all blocks as empty blocks
+     * @param nBlocks: number of blocks to fill
+     */
+    void initialize(int nBlocks);
+    /**
+     * Load all files and directories from VFS.txt, and
+     * Store them in the root directory
+     * @param root: Store all files and directories(subdirectories)
+     */
+    void loadHardDisk(Directory root);
+
+    /**
+     * Save the directory and its files and subdirectories to VFS.txt
+     * @param root: Contains all files and directories(subdirectories)
+     */
+    void saveHardDisk(Directory root);
+
     int allocateFile(_File file);
+
+    void deAllocateFile(_File file);
+
+    void displayDiskStatus();
 }
--- src/VFS/Allocator/Linked.java
+++ src/VFS/Allocator/Linked.java
@@ -1,5 +1,6 @@
 package VFS.Allocator;
 
+import VFS.VFileManager.Directory;
 import VFS.VFileManager._File;
 
 public class Linked implements Allocator{
@@ -7,8 +8,49 @@ public class Linked implements Allocator{
 
     }
 
+    /**
+     * Set all blocks as empty blocks
+     *
+     * @param nBlocks : number of blocks to fill
+     */
+    @Override
+    public void initialize(int nBlocks) {
+
+    }
+
+    /**
+     * Load all files and directories from VFS.txt, and
+     * Store them in the root directory
+     *
+     * @param root : Store all files and directories(subdirectories)
+     */
+    @Override
+    public void loadHardDisk(Directory root) {
+
+    }
+
+    /**
+     * Save the directory and its files and subdirectories to VFS.txt
+     *
+     * @param root : Contains all files and directories(subdirectories)
+     */
+    @Override
+    public void saveHardDisk(Directory root) {
+
+    }
+
     @Override
     public int allocateFile(_File file) {
         return 0;
+    }
+
+    @Override
+    public void deAllocateFile(_File file) {
+
+    }
+
+    @Override
+    public void displayDiskStatus() {
+
     }
 }
